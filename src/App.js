80eb01149b8eb4f955bdabe6683a38b87aa28879
Micaella Melo <135082114+MicasMelo@users.noreply.github.com>
2025-04-03 02:16:54 -0300

    CSS e importação de pacotes
    Definição do body, definição de tabela e aplicação CSS nos componentes;
    
    Instalação e importação dos pacotes bootstrap, axios e reactstrap

--- src/App.js
+++ src/App.js
@@ -1,9 +1,48 @@
-//import logo from './logo.jpg';
+import logo from './logo.jpg';
 import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import React, { useState, useEffect} from "react";
+//import axios from 'axios';
+//import (Modal, ModalBody, ModalFooter, ModelHeader) from 'reactstrap';
 
-//<img src={logo} className="App-logo" alt="logo" />
 
 function App() {
+
+return(
+  <body>
+    <div id="topo">
+      <center><img src={logo} className="App-logo" alt="logo" /></center>
+    </div>
+
+    <div id="conteudo" className="animal-container">
+    <br />
+      <center>
+        <h3><u>Cadastro de Animais</u></h3>
+      </center>
+
+      <table className="table table-bordered">
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Nome</th>
+            <th>Espécie</th>
+            <th>Idade</th>
+            <th>Operação</th>
+          </tr>
+        </thead>
+        <tbody>
+            <tr>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+            </tr>
+        </tbody>
+      </table>
+    </div>
+  </body>
+)
 }
 
 export default App;
